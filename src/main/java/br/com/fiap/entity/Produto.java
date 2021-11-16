fe185e22afeedce66ac0f134f6e0cdbaad265010
Carlos-edward <carlos.eduardo.brito.dev@gmail.com>
2021-11-16 17:44:43 -0300

    table

--- src/main/java/br/com/fiap/entity/Produto.java
+++ src/main/java/br/com/fiap/entity/Produto.java
@@ -1,13 +1,19 @@
 package br.com.fiap.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_produto")
 public class Produto {
 	
 	@Id
+	@SequenceGenerator(name="produto",sequenceName="sq_tb_produto", allocationSize = 2)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "produto")
 	private int id;
 
 }
